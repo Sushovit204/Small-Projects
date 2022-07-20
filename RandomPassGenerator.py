600f783a76aaa1ff65e37dd79864f53e221d84e2
Sushovit <shahsushovit434@gmail.com>
2022-07-20 18:38:58 +0545

    comminting via VS code

--- RandomPassGenerator.py
+++ RandomPassGenerator.py
@@ -1,5 +1,10 @@
 import random
 
+def shuffle(string):
+  tempList = list(string)
+  random.shuffle(tempList)
+  return ''.join(tempList)
+
 uppercase1=chr(random.randint(65, 90))
 uppercase2=chr(random.randint(65, 90))
 
@@ -9,6 +14,9 @@ lowercase2=chr(random.randint(97, 122))
 number1=random.randint(0,9)
 number2=random.randint(0,9)
 
-password=uppercase1+uppercase2+lowercase1+lowercase2+str(number1)+str(number2)
-#random.shuffle(password)
+punctuation1=chr(random.randint(33,38))
+punctuation2=chr(random.randint(33,38))
+
+password=uppercase1+uppercase2+lowercase1+lowercase2+str(number1)+str(number2)+str(punctuation1)+str(punctuation2)
+password=shuffle(password)
 print(password)
